812e959247b02fcaa4bdeaa486539ab08a5b6da2
nexoaiecosystem-coder <nexoaiecosystem@gmail.com>
2026-06-24 03:57:45 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'mimosas-v3';
+const FILES = [
+  '/MIMOSAS/mimosas_v3.html',
+  '/MIMOSAS/manifest.json',
+  '/MIMOSAS/icon-192.png',
+  '/MIMOSAS/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
